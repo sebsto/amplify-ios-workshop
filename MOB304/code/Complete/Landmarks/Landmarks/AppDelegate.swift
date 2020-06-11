675318f3df24b3893ba849e19214ce719a6b7445
Stormacq, Sebastien <stormacq@amazon.lu>
2020-06-11 12:15:07 +0200

    code change : 3. Add Auth

--- MOB304/code/Complete/Landmarks/Landmarks/AppDelegate.swift
+++ MOB304/code/Complete/Landmarks/Landmarks/AppDelegate.swift
@@ -6,12 +6,66 @@ The application delegate.
 */
 
 import UIKit
+import Amplify
+import AmplifyPlugins
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    public let userData = UserData()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        do {
+            Amplify.Logging.logLevel = .info
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            
+            try Amplify.configure()
+            print("Amplify initialized")
+            
+            // load data when user is signedin
+            self.checkUserSignedIn()
+
+            // listen to auth events.
+            // see https://github.com/aws-amplify/amplify-ios/blob/master/Amplify/Categories/Auth/Models/AuthEventName.swift
+            _ = Amplify.Hub.listen(to: .auth) { (payload) in
+
+                switch payload.eventName {
+
+                case HubPayload.EventName.Auth.signedIn:
+                    print("==HUB== User signed In, update UI")
+
+                    self.updateUI(forSignInStatus: true)
+
+                    // if you want to get user attributes
+                    _ = Amplify.Auth.fetchUserAttributes() { (result) in
+                        switch result {
+                        case .success(let attributes):
+                            print("User attribtues - \(attributes)")
+                        case .failure(let error):
+                            print("Fetching user attributes failed with error \(error)")
+                        }
+                    }
+
+
+                case HubPayload.EventName.Auth.signedOut:
+                    print("==HUB== User signed Out, update UI")
+                    self.updateUI(forSignInStatus: false)
+                    
+                case HubPayload.EventName.Auth.sessionExpired:
+                    print("==HUB== Session expired, show sign in aui")
+                    self.updateUI(forSignInStatus: false)
+
+                default:
+                    //print("==HUB== \(payload)")
+                    break
+                }
+            }
+
+        } catch {
+            print("Failed to configure Amplify \(error)")
+        }
+
         return true
     }
 
@@ -31,6 +85,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    // MARK: -- Authentication code
+    
+    // change our internal state, this triggers an UI update on the main thread
+    func updateUI(forSignInStatus : Bool) {
+        DispatchQueue.main.async() {
+            self.userData.isSignedIn = forSignInStatus
+        }
+    }
+    
+    // when user is signed in, fetch its details
+    func checkUserSignedIn() {
+
+        // every time auth status changes, let's check if user is signedIn or not
+        // updating userData will automatically update the UI
+        _ = Amplify.Auth.fetchAuthSession { (result) in
+
+            do {
+                let session = try result.get()
+                self.updateUI(forSignInStatus: session.isSignedIn)
+            } catch {
+                print("Fetch auth session failed with error - \(error)")
+            }
+
+        }
+    }
+    
+    // signin with Cognito web user interface
+    public func authenticateWithHostedUI() {
+
+        print("hostedUI()")
+        _ = Amplify.Auth.signInWithWebUI(presentationAnchor: UIApplication.shared.windows.first!) { result in
+            switch result {
+            case .success(_):
+                print("Sign in succeeded")
+            case .failure(let error):
+                print("Sign in failed \(error)")
+            }
+        }
+    }
+    
+    // signout globally
+    public func signOut() {
+
+        // https://docs.amplify.aws/lib/auth/signOut/q/platform/ios
+        let options = AuthSignOutRequest.Options(globalSignOut: true)
+        _ = Amplify.Auth.signOut(options: options) { (result) in
+            switch result {
+            case .success:
+                print("Successfully signed out")
+            case .failure(let error):
+                print("Sign out failed with error \(error)")
+            }
+        }
+    }
 
 }
-
--- MOB304/code/Complete/Landmarks/Landmarks/Models/UserData.swift
+++ MOB304/code/Complete/Landmarks/Landmarks/Models/UserData.swift
@@ -1,14 +1,9 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A model object that stores app data.
-*/
-
+// Landmarks/Models/UserData.swift
 import Combine
 import SwiftUI
 
 final class UserData: ObservableObject {
     @Published var showFavoritesOnly = false
     @Published var landmarks = landmarkData
+    @Published var isSignedIn : Bool = false
 }

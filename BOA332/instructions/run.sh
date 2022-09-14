761aa8c4f9072099d0c3ad4eb81250a63e6436d0
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-14 20:48:20 +0100

    update scripts to run on latest Hugo

--- BOA332/instructions/run.sh
+++ BOA332/instructions/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:latest  servedocker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:latest  serverr
+docker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:latest  server

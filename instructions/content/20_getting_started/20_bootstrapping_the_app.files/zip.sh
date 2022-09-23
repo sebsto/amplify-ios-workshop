c4745acb1f238485459c03f2dc24ecb18182f372
MAHAMAN <claouan@88665a47a374.ant.amazon.com>
2022-09-23 12:11:06 +0400

    Delete old content folder from instructions folder

--- instructions/content/20_getting_started/20_bootstrapping_the_app.files/zip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-unzip HandlingUserInput.zip
-cp ../../../../code/scripts/* scripts 
-zip -r -X HandlingUserInput.zip Complete/ scripts/ ProjectFiles.md
-rm -r Complete/ ProjectFiles.md  scripts/

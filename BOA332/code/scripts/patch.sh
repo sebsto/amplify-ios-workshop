2b855a57b0c6aacd831f46609a11392743e478fa
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-30 12:17:37 -0400

    Update project starting point

--- BOA332/code/scripts/patch.sh
+++
@@ -1,3 +0,0 @@
-diff -ruN ./Pods/Amplify ~/Documents/amazon/te/2019/reinvent/mobile\ workshop/workshop/BOA302/code/Complete/Landmarks/Pods/Amplify > patch1.txt 
-
-diff -ruN ./Pods/AWSPluginsCore ~/Documents/amazon/te/2019/reinvent/mobile\ workshop/workshop/BOA302/code/Complete/Landmarks/Pods/AWSPluginsCore > patch2.txt

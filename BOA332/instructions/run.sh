992df06dd8b4780f7f0230d23904e55aa4cc7da6
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-10-06 10:18:35 +0200

    use various hugo docker images

--- BOA332/instructions/run.sh
+++ BOA332/instructions/run.sh
@@ -1,1 +1,2 @@
-docker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:latest  server
+docker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:0.101.0-busybox  server
+#docker run --rm -it -v "$(pwd)":/src  -p 1313:1313 klakegg/hugo:0.101.0-ubuntu  server

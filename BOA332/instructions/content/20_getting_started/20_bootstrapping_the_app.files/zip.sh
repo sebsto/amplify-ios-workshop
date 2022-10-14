a23f78adc8e26b5fe2bb845d101dba9259b6d1cd
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-10-14 12:17:46 +0200

    rename BOA332 directory to workshop

--- BOA332/instructions/content/20_getting_started/20_bootstrapping_the_app.files/zip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-unzip HandlingUserInput.zip
-cp ../../../../code/scripts/* amplify-ios-workshop/scripts 
-zip -r -X HandlingUserInput.zip amplify-ios-workshop/*
-rm -r amplify-ios-workshop
